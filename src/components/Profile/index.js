f4825a3752d330a19aceed94e70dc6e72d611571
Jack <jack.morgan.edwards@gmail.com>
2022-09-13 11:59:43 -0400

    overflow issue with projects page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,15 @@
 function Profile() {
-  return <div>Profile Stuff</div>;
+  return (
+    <div className="App">
+      <div className="profile--main">
+        <p>
+          Graduate of Ithaca College with a passion for generative electronic
+          music systems and interactive web-based audio apps with intuitive
+          design.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
