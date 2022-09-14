e0839efab27e24e68f2fc811b23eac414edf55f6
Jack <jack.morgan.edwards@gmail.com>
2022-09-14 16:03:23 -0400

    fade in

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -9,13 +9,13 @@ function Profile() {
   return (
     <div className="App">
       <div className="profile--main">
-        <p>
+        <p className="fade--in--1">
           Graduate of Ithaca College with a passion for generative electronic
           music systems and interactive web-based audio apps with intuitive
           design.
         </p>
-        <h1>Check out some of my Projects:</h1>
-        {projects}
+        <h1 className="fade--in--1">Check out some of my Projects:</h1>
+        <div className="fade--in--2">{projects}</div>
       </div>
     </div>
   );
