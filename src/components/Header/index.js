09835c8e8c314a7bc5027de8a73431742391cc15
Jack <jack.morgan.edwards@gmail.com>
2022-09-17 20:05:25 -0400

    web test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,8 +29,8 @@ function Header() {
         <h4>Jack Edwards</h4>
       </div>
       <div className="header-sub">
-        {pathname !== "/portfolio-site" && (
-          <NavLink to="/portfolio-site">About</NavLink>
+        {pathname !== "/portfolio-site/" && (
+          <NavLink to="/portfolio-site/">About</NavLink>
         )}
         {pathname !== "/projects" && <NavLink to="/projects">Projects</NavLink>}
         {pathname !== "/resume" && <NavLink to="/resume">Resume</NavLink>}
